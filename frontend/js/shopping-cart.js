35745f85ddb6d29d3038eff9db96d40fb104d600
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-04-08 20:34:10 +0200

    Started on bdd-tests and gherkin features

--- frontend/js/shopping-cart.js
+++ frontend/js/shopping-cart.js
@@ -16,31 +16,39 @@ class ShoppingCart {
       this.render();
   }
 
-  render() {
-    // create a html table where we display
-    // the order rows of the shopping cart
-    let html = '<table class="shoppingList">';
-    let totalSum = 0;
-    for (let orderRow of this.orderRows) {
-      let rowSum =
-        orderRow.quantity * orderRow.product.price;
-      html += `
-        <tr>
-          <td>${orderRow.quantity}</td>
-          <td>${orderRow.product.name}</td>
-          <td>${orderRow.product.price}</td>
-          <td>${rowSum}</td>
-        </tr>
-      `;
-      totalSum += rowSum;
-    }
-    // add the totalSum
-    html += `<tr>
-      <td colspan="3">Total:</td>
-      <td>${totalSum}</td>
-    </tr>`;
-    html += '</table>';
-    return html;
-  }
+
+	formatSEK(number)
+	{
+		return new Intl.NumberFormat( 'sv-SE', { style: 'currency', currency: 'SEK' } ).format( number );
+	}
+
+
+	render()
+	{
+		// create a html table where we display
+		// the order rows of the shopping cart
+		let html = '<table class="shoppingList">';
+		let totalSum = 0;
+		for (let orderRow of this.orderRows) {
+			let rowSum =
+			orderRow.quantity * orderRow.product.price;
+			html += `
+			<tr>
+				<td>${ orderRow.quantity }</td>
+				<td>${ orderRow.product.name }</td>
+				<td>${ this.formatSEK( orderRow.product.price ) }</td>
+				<td>${ this.formatSEK( rowSum ) }</td>
+			</tr>
+			`;
+			totalSum += rowSum;
+		}
+		// add the totalSum
+		html += `<tr>
+			<td colspan="3">Total:</td>
+			<td>${  this.formatSEK( totalSum ) }</td>
+		</tr>`;
+		html += '</table>';
+		return html;
+	}
 
 }
